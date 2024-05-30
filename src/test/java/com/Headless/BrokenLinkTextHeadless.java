6a45e7722cbef7c2b7eb36372850ce419f31807b
Agboola Michael Daramola <61335052+Mivics1@users.noreply.github.com>
2024-05-30 14:55:39 +0100

    Using HtmlUnitDriver for headless mode

---
+++ src/test/java/com/Headless/BrokenLinkTextHeadless.java
@@ -0,0 +1,67 @@
+package com.Headless;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class BrokenLinkTextHeadless {
+	
+	public static WebDriver driver;
+
+	List<WebElement> activeLinks = new ArrayList<WebElement>();
+	
+	@BeforeTest
+	public void openBrowser() {
+		driver = new HtmlUnitDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+	}
+
+	@Test
+	public void getLinks() {
+		
+		//List for all links and image
+		List<WebElement> linkImgList = driver.findElements(By.tagName("a"));
+		linkImgList.addAll(driver.findElements(By.tagName("img")));
+		
+		//Initial total Links and Images
+		int initialTotal = linkImgList.size();
+		System.out.println("Initially, We have a total of: "+initialTotal+" link(s) present in the webpage");
+		
+		for(int num=0; num<linkImgList.size(); num++) {
+			if(linkImgList.get(num).getAttribute("href") != null && (!linkImgList.get(num).getAttribute("href").contains("JavaScript"))) {
+				activeLinks.add(linkImgList.get(num));
+			}
+			
+		}
+		//Final total Links and Images
+		int finalTotal = activeLinks.size();
+		System.out.println("Finally, we have a total of "+finalTotal+" link(s) present in the webpage");
+		}
+	
+	//
+	@Test
+	public void verifyBrokenLinksHeadless() throws IOException {
+		VerifyBrokenLinkHeadless obj = new VerifyBrokenLinkHeadless();
+		for(int num=0; num<activeLinks.size();num++) {
+			WebElement ele = activeLinks.get(num);
+			String url = ele.getAttribute("href");
+			obj.verifyBrokenLinks(url);
+		}
+		System.err.println("Total Valid Links: "+obj.validLinks);
+		System.err.println("Total Invalid Links: "+obj.invalidLinks);
+	}
+	@AfterTest
+	public void tearDown() {
+		driver.close();
+	}
+}
