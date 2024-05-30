8a0743ce42d0373d6b1a6ba886fdb9a6988cf7c2
Agboola Michael Daramola <61335052+Mivics1@users.noreply.github.com>
2024-05-30 13:04:31 +0100

    SEL-2 utility class for verification of broken links

--- src/test/java/com/MavenTestPackage/VerifyBrokenLinkChrome.java
+++ src/test/java/com/MavenTestPackage/VerifyBrokenLinkChrome.java
@@ -1,8 +1,35 @@
 package com.MavenTestPackage;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class VerifyBrokenLinkChrome {
-
+	int validLinks = 0;
+	int invalidLinks = 0;
+	
+	public void verifyBrokenLinks(String linkUrl) throws IOException {
+		
+		try {
+			URL url = new URL(linkUrl);
+			HttpURLConnection httpUrlConnect = (HttpURLConnection) url.openConnection();
+			httpUrlConnect.connect();
+			
+			int resCode = httpUrlConnect.getResponseCode();
+			
+			if (resCode >= 400) {
+				System.out.println(linkUrl+" : is a broken link"+"---"+httpUrlConnect.getResponseMessage()+"---"+httpUrlConnect.getResponseCode());
+				invalidLinks=invalidLinks + 1;
+			} else {
+				System.out.println(url+" : is a broken link"+"---"+httpUrlConnect.getResponseMessage()+"---"+httpUrlConnect.getResponseCode());
+				validLinks = validLinks +1;
+			}
+			httpUrlConnect.disconnect();
+		} catch(MalformedURLException e){
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
